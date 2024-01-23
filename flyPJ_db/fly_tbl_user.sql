0b95e7eff59d86f828235f07f3feaa96d0fe969c
vu tran <quachvotinh006@gmail.com>
2024-01-23 22:58:15 +0900

    d

--- flyPJ_db/fly_tbl_user.sql
+++ flyPJ_db/fly_tbl_user.sql
@@ -1,26 +1,16 @@
+-- flyPJ_db/fly_tbl_user.sql
+
 DROP TABLE IF EXISTS `tbl_user`;
 
 CREATE TABLE `tbl_user` (
-  `ID` bigint NOT NULL AUTO_INCREMENT,
+  `ID` int NOT NULL AUTO_INCREMENT,
+  `FULL_NAME` varchar(128) NOT NULL,
   `EMAIL` varchar(256) NOT NULL,
   `PASSWORD` varchar(256) NOT NULL,
-  `FIRSTNAME` varchar(128) NOT NULL,
-  `LASTNAME` varchar(128) NOT NULL,
-  `AGE` int,
-  `AVATAR_LINK` varchar(256),
-  `CREATED_AT` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-  `DELETED_AT` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `STAGE` tinyint CHECK (`STAGE` IN (0, 1, 2)),
-  `ACTIVE_CODE` varchar(128),
-  `TOKEN` varchar(256),
   PRIMARY KEY (`ID`),
   UNIQUE KEY `EMAIL` (`EMAIL`),
-  CONSTRAINT `tbl_user_chk_1` CHECK (((length(`FIRSTNAME`) >= 6) and (length(`FIRSTNAME`) <= 64))),
+  CONSTRAINT `tbl_user_chk_1` CHECK (((length(`FULL_NAME`) >= 6) and (length(`FULL_NAME`) <= 64))),
   CONSTRAINT `tbl_user_chk_2` CHECK (((length(`EMAIL`) >= 6) and (length(`EMAIL`) <= 256)))
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-LOCK TABLES `tbl_user` WRITE;
-INSERT INTO `tbl_user` (`EMAIL`, `FIRSTNAME`, `LASTNAME`, `AGE`, `AVATAR_LINK`, `STAGE`, `ACTIVE_CODE`, `TOKEN`)
-VALUES
-  ('test@example.com', 'anhlacho12','Test', 'User', 25, 'https://example.com/avatar.jpg', 2, '123456', 'abc123def456');
-UNLOCK TABLES;
+
